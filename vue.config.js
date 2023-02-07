42edc65e30e12ba8f9c7c7c5475e5734bb3fc170
Andrei Danila <acdnuclear@gmail.com>
2023-02-07 21:38:54 +0200

    ip changes

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,4 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-module.exports = {
-  devServer: {
-    port: 8080,
-    host: '10.0.6.33'
-  }
-}
+
